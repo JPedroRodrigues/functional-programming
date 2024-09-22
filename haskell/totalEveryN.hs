17bb34de9b1361fcad6001e46ebf59ed48642740
João Pedro <jpedrovieira34@gmail.com>
2024-09-22 16:05:33 -0300

    update totalEveryN.hs

--- haskell/totalEveryN.hs
+++ haskell/totalEveryN.hs
@@ -1,6 +1,7 @@
 -- Input sample: 5 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 -- Output expected: [5, 5, 2]
 
+totK _ [] = []
 totK k (a : xs) = total k 0 0 (a : xs)
 
 total _ _ totalSum [] = [totalSum]
